2417a0e1c1b991d33140075f66b589f637389067
양원준 <97458548+ywj9811@users.noreply.github.com>
2023-02-16 04:21:07 +0900

    feat: Redis 사용 위한 Dependency 추가 및 설정

---
+++ src/main/java/com/example/oauth2WithJwt/repository/RedisRepo.java
@@ -0,0 +1,33 @@
+package com.example.oauth2WithJwt.repository;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.stereotype.Repository;
+
+import java.time.Duration;
+
+@RequiredArgsConstructor
+@Repository
+public class RedisRepo {
+    private final RedisTemplate<String, String> redisTemplate;
+
+    public void setValues(String key, String data) {
+        ValueOperations<String, String> values = redisTemplate.opsForValue();
+        values.set(key, data);
+    }
+
+    public void setValues(String key, String data, Duration duration) {
+        ValueOperations<String, String> values = redisTemplate.opsForValue();
+        values.set(key, data, duration);
+    }
+
+    public String getValues(String key) {
+        ValueOperations<String, String> values = redisTemplate.opsForValue();
+        return values.get(key);
+    }
+
+    public void deleteValues(String key) {
+        redisTemplate.delete(key);
+    }
+}
